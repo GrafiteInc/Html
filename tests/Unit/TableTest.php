36d081845efd14db5a5c33ab279bcaffde36b1df
Matt Lantz <mattlantz@gmail.com>
2022-02-08 16:51:49 -0500

    because of Laravel 9

--- tests/Unit/TableTest.php
+++ tests/Unit/TableTest.php
@@ -8,9 +8,11 @@ class TableTest extends TestCase
 {
     public function testHtmlRendering()
     {
-        // $html = \Grafite\Html\Tags\Table::make()->sortable('window.app.processFilters')->headers(['employee number' => 'id', 'title' => 'job', 'Name' => 'name', 'Actions' => null])->collection(collect([
-        //     (object) ['id' => 1, 'job' => 'bar', 'name' => 'baz', 'actions' => '<button class="btn btn-sm btn-info">Now</button>'],
-        //     (object) ['id' => 2, 'job' => 'janitor', 'name' => 'jim', 'actions' => '<a>What</a>'],
-        // ]))->render();
+        $html = \Grafite\Html\Tags\Table::make()->sortable('window.app.processFilters')->headers(['employee number' => 'id', 'title' => 'job', 'Name' => 'name', 'Actions' => null])->collection(collect([
+            (object) ['id' => 1, 'job' => 'bar', 'name' => 'baz', 'actions' => '<button class="btn btn-sm btn-info">Now</button>'],
+            (object) ['id' => 2, 'job' => 'janitor', 'name' => 'jim', 'actions' => '<a>What</a>'],
+        ]))->render();
+
+        $this->assertStringContainsString('<span class="html-component-pointer" onclick="window.app.processFilters(\'id\', \'asc\')">Employee Number', $html);
     }
 }
