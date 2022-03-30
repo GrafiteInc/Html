f4c1a59605ee18dd174d88dce85f269d7aca0e15
Matt Lantz <mattlantz@gmail.com>
2022-03-30 08:56:09 -0400

    because offcanvas components are super cool

---
+++ src/Tags/OffCanvas.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Grafite\Html\Tags;
+
+use Illuminate\Support\Str;
+
+class OffCanvas extends HtmlComponent
+{
+    public static $position;
+
+    public static function position($position)
+    {
+        self::$position = $position;
+
+        return new static;
+    }
+
+    public static function process()
+    {
+        $css = self::$css ?? '';
+        $text = self::$text ?? '';
+        $position = self::$position ?? 'end';
+        $id = self::$id ?? 'html_component_offcanvas_' . Str::uuid();
+
+
+        self::$html = <<<html
+            <div id="offCanvas_{$id}" class="offcanvas offcanvas-{$position}" ref="offcanvas-panel" tabindex="-1" aria-labelledby="offcanvasLabel_{$id}">
+                <div class="offcanvas-header">
+                    <h5 id="offcanvasLabel_{$id}">{$text}</h5>
+                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    {{ \$slot }}
+                </div>
+            </div>
+            <button class="{$css}" type="button" data-bs-toggle="offcanvas" data-bs-target="#offCanvas_{$id}" aria-controls="offCanvas_{$id}">
+                {$text}
+            </button>
+html;
+    }
+}
