14844b15f63f77f66dbcb40e8842dd5b51eeb7b9
Matt Lantz <mattlantz@gmail.com>
2023-05-27 23:45:42 -0400

    because of a map engine

---
+++ src/Tags/Map.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Grafite\Html\Tags;
+
+use Illuminate\Support\Str;
+use Grafite\Html\Tags\HtmlComponent;
+
+class Map extends HtmlComponent
+{
+    public static $marker;
+    public static $center;
+    public static $bubbles;
+    public static $zoom = 13;
+    public static $maxZoom = 19;
+    public static $skin = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+    public static function skin($value)
+    {
+        self::$skin = $value;
+
+        return new static;
+    }
+
+    public static function bubbles($array)
+    {
+        foreach($array as $bubble) {
+            $var = 'bubbles_'.Str::random();
+            $x = $bubble['x'];
+            $y = $bubble['y'];
+            $color = $bubble['color'] ?? 'var(--bs-primary)';
+            $fill = $bubble['fill'] ?? 'var(--bs-primary)';
+            $opacity = $bubble['opacity'] ?? 1.0;
+            $radius = $bubble['radius'] ?? 10;
+            $tooltip = $bubble['tooltip'] ?? null;
+            $click = $bubble['click'] ?? null;
+
+            self::$bubbles .= "var {$var} = L.circle([{$x}, {$y}], {
+                color: '{$color}',
+                    fillColor: '{$fill}',
+                    fillOpacity: {$opacity},
+                    radius: {$radius}
+                }).addTo(map); ";
+
+            if (! is_null($tooltip)) {
+                self::$bubbles .= " {$var}.bindPopup(\"{$tooltip}\").openPopup();";
+            }
+
+            if (! is_null($click)) {
+                self::$bubbles .= " {$var}.on('click', function () { {$click}; });";
+            }
+        }
+
+        return new static;
+    }
+
+
+    public static function zoom($value)
+    {
+        self::$zoom = $value;
+
+        return new static;
+    }
+
+    public static function maxZoom($value)
+    {
+        self::$maxZoom = $value;
+
+        return new static;
+    }
+
+    public static function center($x, $y)
+    {
+        self::$center = "[$x, $y]";
+
+        return new static;
+    }
+
+    public static function marker($x, $y, $tooltip = null, $click = null)
+    {
+        $var = 'marker_'.Str::random();
+
+        self::$marker = "var {$var} = L.marker([{$x}, {$y}]).addTo(map);";
+
+        if (! is_null($tooltip)) {
+            self::$marker .= " {$var}.bindPopup(\"{$tooltip}\").openPopup();";
+        }
+
+        if (! is_null($click)) {
+            self::$marker .= " {$var}.on('click', function () { {$click}; });";
+        }
+
+        return new static;
+    }
+
+    public static function stylesheets()
+    {
+        return [
+            '//unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+        ];
+    }
+
+    public static function scripts()
+    {
+        return [
+            '//unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+        ];
+    }
+
+    public static function js()
+    {
+        $id = self::$id;
+        $marker = self::$marker;
+        $center = self::$center;
+        $skin = self::$skin;
+        $zoom = self::$zoom;
+        $maxZoom = self::$maxZoom;
+        $bubbles = self::$bubbles;
+
+        return <<<JS
+           document.addEventListener('DOMContentLoaded', (event) => {
+                var map = L.map('{$id}').setView({$center}, $zoom);
+                L.tileLayer('{$skin}', {
+                    maxZoom: $maxZoom,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                }).addTo(map);
+
+                {$marker}
+                {$bubbles}
+            });
+        JS;
+    }
+
+    public static function styles()
+    {
+        return <<<CSS
+            .leaflet-map {
+                min-height: 400px;
+            }
+        CSS;
+    }
+
+    public static function process()
+    {
+        self::$id = self::$id ?? 'html_'.Str::uuid();
+
+        $id = self::$id;
+
+        self::$html = "<div id=\"{$id}\" class=\"leaflet-map\"></div>";
+    }
+}
---
+++ tests/Unit/MapTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Grafite\Html\Tags\Map;
+use Grafite\Html\HtmlAssets;
+
+class MapTest extends TestCase
+{
+    public function testHtmlRendering()
+    {
+        $html = Map::make()
+            ->center(43.981739, -80.735542)
+            ->marker(43.981739, -80.735542, 'This is my home')
+            ->render();
+
+        $scripts = app(HtmlAssets::class)->render();
+
+        $this->assertStringContainsString('class="leaflet-map"', $html);
+        $this->assertStringContainsString('.setView([43.981739, -80.735542]', $scripts);
+        $this->assertStringContainsString('This is my home', $scripts);
+    }
+}
